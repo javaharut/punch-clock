cf7ad4934179e834a91e1fe177a140b45d67d179
Søren Mortensen <soren@neros.dev>
2019-12-26 14:43:39 -0500

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+//  lib.rs
+//  punch
+//
+//  Created by Søren Mortensen <soren@neros.dev> on 2019-12-26.
+//  Copyright (c) 2019 Søren Mortensen.
+//
+//  Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+//  http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+//  http://opensource.org/licenses/MIT>, at your option. This file may not be
+//  copied, modified, or distributed except according to those terms.
+
+pub fn hello_world() {
+    println!("Hello, world!");
+}
