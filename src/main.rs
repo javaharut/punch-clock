8274e509a59525dde4d8e66a1bec81ccc4130850
Søren Mortensen <soren@neros.dev>
2020-02-29 19:22:31 +0000

    Remove config-related things

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@
 //  http://opensource.org/licenses/MIT>, at your option. This file may not be
 //  copied, modified, or distributed except according to those terms.
 
-mod config;
 mod opt;
 
 use chrono::Local;
