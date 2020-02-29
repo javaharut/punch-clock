33e417b714fd24f59af9462e4c4e72023e4c224f
Søren Mortensen <soren@neros.dev>
2020-02-29 19:13:21 +0000

    Improve handling of count time periods

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,11 @@
 use chrono::{DateTime, Local};
 use structopt::StructOpt;
 
+use std::{
+    fmt::{Display, Formatter, Result as FmtResult},
+    str::FromStr,
+};
+
 mod config;
 
 #[derive(Debug, StructOpt)]
@@ -39,10 +44,54 @@ enum Opt {
     Count {
         /// Period of time to count from.
         ///
-        /// Values for <period> include: today, yesterday, week, month, last week, last month.
-        /// Shortened versions of these values are also available, such as "t" for "today".
-        period: String,
+        /// Values for <period> include: all, today, yesterday, week, month, last week,
+        /// last month. Shortened versions of these values are also available, such as "t" for
+        /// "today".
+        #[structopt(default_value = "today")]
+        period: Period,
     },
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum Period {
+    All,
+    Today,
+    Yesterday,
+    Week,
+    LastWeek,
+    Month,
+    LastMonth,
+}
+
+impl FromStr for Period {
+    type Err = String;
+
+    fn from_str(raw: &str) -> Result<Self, Self::Err> {
+        match raw {
+            "all" | "a" => Ok(Period::All),
+            "today" | "t" => Ok(Period::Today),
+            "yesterday" | "y" => Ok(Period::Yesterday),
+            "week" | "this week" | "w" | "tw" => Ok(Period::Week),
+            "last week" | "lastweek" | "lw" => Ok(Period::LastWeek),
+            "month" | "this month" | "m" | "tm" => Ok(Period::Month),
+            "last month" | "lastmonth" | "lm" => Ok(Period::LastMonth),
+            _ => Err("Time period not recognised.".into()),
+        }
+    }
+}
+
+impl Display for Period {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        match self {
+            Period::All => write!(f, "All-Time"),
+            Period::Today => write!(f, "Today"),
+            Period::Yesterday => write!(f, "Yesterday"),
+            Period::Week => write!(f, "This Week"),
+            Period::LastWeek => write!(f, "Last Week"),
+            Period::Month => write!(f, "This Month"),
+            Period::LastMonth => write!(f, "Last Month"),
+        }
+    }
 }
 
 fn main() {
@@ -63,20 +112,7 @@ fn main() {
             println!("Not tracking time.");
         }
         Opt::Count { period } => {
-            let pretty_period = match &*period.to_lowercase() {
-                "today" | "t" => "today",
-                "yesterday" | "y" => "yesterday",
-                "week" | "this week" | "w" => "this week",
-                "last week" | "lastweek" | "lw" => "last week",
-                "month" | "this month" | "m" => "this month",
-                "last month" | "lastmonth" | "lm" => "last month",
-                _ => {
-                    eprintln!("Unrecognised period \"{}\".", period);
-                    return;
-                }
-            };
-
-            println!("Time worked {}: none.", pretty_period);
+            println!("Time worked {}: none.", period.to_string().to_lowercase());
         }
     }
 }
