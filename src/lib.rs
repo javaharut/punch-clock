91bd1512a8e98b617a740cf7bb0b2f0374aef52a
Søren Mortensen <soren@neros.dev>
2020-03-16 03:33:09 +0000

    Add documentation to the crate root

--- src/lib.rs
+++ src/lib.rs
@@ -9,8 +9,13 @@
 //  http://opensource.org/licenses/MIT>, at your option. This file may not be
 //  copied, modified, or distributed except according to those terms.
 
-pub mod event;
-pub mod period;
+//! The `punch-clock` crate is a lightweight tool for tracking time.
+//!
+//! This library exposes an API for performing all the same tasks as through the command-line
+//! interface (e.g. punching in or out, checking time tracking status, counting totals).
+
+mod event;
+mod period;
 pub mod sheet;
 
 pub use event::Event;
